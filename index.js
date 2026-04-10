fc5ebab174d975fa8cf80c91465f4b665c3333bf
Vinícius Brandão <viniciusbrandao9417@gmail.com>
2026-04-10 17:31:03 -0300

    feat: O usário ao botar o peso e sua altura, quando clica no botão ele informa o seu IMC.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const btnEl = document.getElementById("btn");
-console.log(btnEl);
+
+
 
 
 function calcularImc() {
-    
+    const alturaValor = document.getElementById("altura").value;
+    const pesoValor = document.getElementById("peso").value;
+    const IMC = pesoValor / ((alturaValor/100)**2);
+    const imcFormatado = IMC.toFixed(2);
+    const seuIMC = document.getElementById("resultado-imc").value = imcFormatado;
 }
 
 btnEl.addEventListener("click", () => {
