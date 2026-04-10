bf92f96a5cfc6565d0cda983c985c15fd54891cd
Vinícius Brandão <viniciusbrandao9417@gmail.com>
2026-04-10 18:52:41 -0300

    feat: finaliza lógica da calculadora de IMC com validação e feedback visual
    - Adicionado cálculo de IMC com conversão de cm para metros
    - Implementado arredondamento com toFixed(2)
    - Criada estrutura de if/else if para classificação de peso
    - Adicionada alteração dinâmica de cores via JS
    - Inserida validação de campos vazios

--- index.js
+++ index.js
@@ -8,15 +8,18 @@ function calcularImc() {
     const IMC = pesoValor / ((alturaValor / 100) ** 2);
     const imcFormatado = IMC.toFixed(2);
     document.getElementById("resultado-imc").value = imcFormatado;
+    if (alturaValor === "" || pesoValor === "") {
+        alert("Opa! Você esqueceu de preencher o peso ou a altura.");
+    }
     if (imcFormatado < 18.5) {
         mensagemCondicao.innerText = "Abaixo do peso";
         mensagemCondicao.style.color = "blue";
-    }else if (imcFormatado <= 24.9) {
+    } else if (imcFormatado <= 24.9) {
         mensagemCondicao.innerText = "Peso normal";
         mensagemCondicao.style.color = "green";
     } else if (imcFormatado <= 29.9) {
         mensagemCondicao.innerText = "Sobrepeso";
-        mensagemCondicao.style.color = "darkorange";
+        mensagemCondicao.style.color = "yellow";
     } else if (imcFormatado <= 35) {
         mensagemCondicao.innerText = "Obesidade";
         mensagemCondicao.style.color = "orange";
