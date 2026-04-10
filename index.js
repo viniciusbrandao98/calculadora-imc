a436149a610b9e2f48765c468d2311a381539cf6
Vinícius Brandão <viniciusbrandao9417@gmail.com>
2026-04-10 18:30:55 -0300

    feat: Ajustes de formatação corrigidos.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const btnEl = document.getElementById("btn");
-const mensagemCondicao = document.getElementById("condicao-peso")
-console.log(mensagemCondicao);
+const mensagemCondicao = document.getElementById("condicao-peso");
 
 
 function calcularImc() {
@@ -8,27 +7,28 @@ function calcularImc() {
     const pesoValor = document.getElementById("peso").value;
     const IMC = pesoValor / ((alturaValor / 100) ** 2);
     const imcFormatado = IMC.toFixed(2);
-    const seuIMC = document.getElementById("resultado-imc").value = imcFormatado;
+    document.getElementById("resultado-imc").value = imcFormatado;
     if (imcFormatado < 18.5) {
-        mensagemCondicao.innerText = "Abaixo do peso"
-        mensagemCondicao.style.color = "lightblue"
+        mensagemCondicao.innerText = "Abaixo do peso";
+        mensagemCondicao.style.color = "blue";
     }else if (imcFormatado <= 24.9) {
-        mensagemCondicao.innerText = "Peso normal"
-        mensagemCondicao.style.color = "green"
+        mensagemCondicao.innerText = "Peso normal";
+        mensagemCondicao.style.color = "green";
     } else if (imcFormatado <= 29.9) {
-        mensagemCondicao.innerText = "Sobrepeso"
-        mensagemCondicao.style.color = "yellow"
+        mensagemCondicao.innerText = "Sobrepeso";
+        mensagemCondicao.style.color = "darkorange";
     } else if (imcFormatado <= 35) {
-        mensagemCondicao.innerText = "Obesidade"
-        mensagemCondicao.style.color = "orange"
+        mensagemCondicao.innerText = "Obesidade";
+        mensagemCondicao.style.color = "orange";
     } else {
-        mensagemCondicao.innerText = "Obesidade extrema"
-        mensagemCondicao.style.color = "red"
+        mensagemCondicao.innerText = "Obesidade extrema";
+        mensagemCondicao.style.color = "red";
     }
+
 }
 
 
 btnEl.addEventListener("click", () => {
-    calcularImc()
+    calcularImc();
 });
 
